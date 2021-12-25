4c3deeba5382f9be28d02216de79e984248e00b3
Adartho <64917020+Adartho@users.noreply.github.com>
2021-12-25 10:51:34 +0000

    trollus

---
+++ common/defines/00_famana.lua
@@ -0,0 +1,83 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NGame.START_DATE = "212.6.6"
+NDefines.NGame.END_DATE = "2639.11.25" --please fix, this is just for timelapses
+NDefines.NGame.MAX_RANDOM_NEW_WORLD = "1.1.1"
+
+NDefines.NReligion.MAX_OPPIAN_RELIGIOUS_CENTERS = 3
+
+NDefines.NReligion.CONSECRATE_PATRIACH_AUTHORITY_BOOST = 0.05 --5% or so
+NDefines.NReligion.CONSECRATE_PATRIARCH_THRESHOLD = 30 --in development
+NDefines.NReligion.ORTHODOX_ICON_DURATION_MONTHS = 120
+NDefines.NReligion.ORTHODOX_ICON_AUTHORITY_COST = 0.0
+
+NDefines.NFrontend.CAMERA_LOOKAT_X = 1500.0
+NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 1000.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1500.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Y = 0.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 1000.0
+NDefines.NFrontend.CAMERA_START_X = 1500.0
+NDefines.NFrontend.CAMERA_START_Y = 800
+NDefines.NFrontend.CAMERA_START_Z = 900.0
+NDefines.NFrontend.CAMERA_END_X = 1500.0
+NDefines.NFrontend.CAMERA_END_Y = 900.0
+NDefines.NFrontend.CAMERA_END_Z = 900.0
+NDefines.NFrontend.CAMERA_MIN_DIST_FOR_ROTATE = 800.0
+NDefines.NFrontend.CAMERA_MIN_HEIGHT = 50.0
+NDefines.NFrontend.CAMERA_MAX_HEIGHT = 3000.0
+NDefines.NFrontend.TIME_FROZEN = 1.0
+NDefines.NFrontend.TIME_UNTIL_ROTATE = 1.5
+NDefines.NFrontend.CAMERA_SPEED_START = 0.04
+NDefines.NFrontend.CAMERA_SPEED_ROTATE = 0.04
+NDefines.NFrontend.GUI_MOVE_SPEED = 300
+
+NDefines.NFrontend.FADE_IN_DONE_TIME = 2.2
+NDefines.NFrontend.GUI_START_MOVE_TIME = 1.4
+NDefines.NFrontend.CAMERA_START_MOVE_TIME = 3.0
+
+NDefines.NFrontend.CAMERA_SPEED_IN_MENUS = 0.1
+
+NDefines.NFrontend.FRONTEND_POS_X = 1500.0
+NDefines.NFrontend.FRONTEND_POS_Y = 900.0
+NDefines.NFrontend.FRONTEND_POS_Z = 1000
+NDefines.NFrontend.FRONTEND_LOOK_X = 1500.0
+NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1019
+
+NDefines.NFrontend.SETTINGS_POS_X = 2958.0
+NDefines.NFrontend.SETTINGS_POS_Y = 551.0
+NDefines.NFrontend.SETTINGS_POS_Z = 978.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 2998.0
+NDefines.NFrontend.SETTINGS_LOOK_Y = 0.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 1364.0
+
+NDefines.NFrontend.MP_OPTIONS_POS_X = 2958.0
+NDefines.NFrontend.MP_OPTIONS_POS_Y = 922.0
+NDefines.NFrontend.MP_OPTIONS_POS_Z = 1048.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 2958.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Y = 0.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1159.0
+
+NDefines.NFrontend.TUTORIAL_POS_X = 2894.0
+NDefines.NFrontend.TUTORIAL_POS_Y = 124.0
+NDefines.NFrontend.TUTORIAL_POS_Z = 1319.0
+NDefines.NFrontend.TUTORIAL_LOOK_X = 2881.0
+NDefines.NFrontend.TUTORIAL_LOOK_Y = 21.0
+NDefines.NFrontend.TUTORIAL_LOOK_Z = 1463.0
+
+NDefines.NFrontend.CONTENT_POS_X = 2958.0
+NDefines.NFrontend.CONTENT_POS_Y = 922.0
+NDefines.NFrontend.CONTENT_POS_Z = 1308.0
+NDefines.NFrontend.CONTENT_LOOK_X = 2958.0
+NDefines.NFrontend.CONTENT_LOOK_Y = 0.0
+NDefines.NFrontend.CONTENT_LOOK_Z = 1519.0
+
+NDefines.NFrontend.CREDITS_POS_X = 3077.0
+NDefines.NFrontend.CREDITS_POS_Y = 231.0
+NDefines.NFrontend.CREDITS_POS_Z = 1508.0
+NDefines.NFrontend.CREDITS_LOOK_X = 3080.0
+NDefines.NFrontend.CREDITS_LOOK_Y = 19.0
+NDefines.NFrontend.CREDITS_LOOK_Z = 1720.0
