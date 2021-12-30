513a51a78ecca7f4daa6844be343057ebc21cd5e
Adartho <64917020+Adartho@users.noreply.github.com>
2021-12-30 17:47:52 +0000

    Named Provinces
    Trolled!

--- ProvincesCode.py
+++ ProvincesCode.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
             provinceName = input("Province Name:\t")
             
             #prov_names_l_english
-            f.write(f" PROV{provinceID}:0 {provinceName}\n")
-            f.write(f" PROV{provinceID}_desc:0 {provinceName}\n")
+            f.write(f' PROV{provinceID}:0 "{provinceName}"\n')
+            f.write(f' PROV{provinceID}_desc:0 "{provinceName}"\n')
             try:
                 with open(f"history\provinces\{provinceID} - UNNAMED.txt", "r+") as historyF:
                    strID = historyF.read()
-                   strID.replace('capital = "UNNAMED"',f'capital = "{provinceName}"')
-                   strID.replace('capital = "Unnamed"',f'capital = "{provinceName}"')
-                   strID.replace('capital = "unnamed"',f'capital = "{provinceName}"')
+                   strID = strID.replace('capital = "UNNAMED"',f'capital = "{provinceName}"')
+                   strID = strID.replace('capital = "Unnamed"',f'capital = "{provinceName}"')
+                   strID = strID.replace('capital = "unnamed"',f'capital = "{provinceName}"')
                 with open(f"history\provinces\{provinceID} - UNNAMED.txt", "w") as historyF:
                    historyF.write(strID) #replace city name
             except IOError:
