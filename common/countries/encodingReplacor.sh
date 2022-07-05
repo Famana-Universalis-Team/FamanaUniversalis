373e5e911367d2db2cda907cbbe8f7bff9cde561
adartho <64917020+Adartho@users.noreply.github.com>
2022-07-05 11:13:28 +0000

    why doesnt the fucking encoder work

--- common/countries/encodingReplacor.sh
+++ common/countries/encodingReplacor.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-FROM_ENCODING="UTF-8"
-TO_ENCODING="ISO_8859-1"
+FROM_ENCODING="utf8"
+TO_ENCODING="iso88591"
 #convert
 CONVERT=" iconv  -f   $FROM_ENCODING  -t   $TO_ENCODING"
 #loop to convert multiple files 
 for  file  in  *.txt; do
-     $CONVERT   "$file"   -o  "${file%.txt}.txt"
+     $CONVERT   "$file"
 done
 exit 0
