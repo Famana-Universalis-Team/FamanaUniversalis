c84d4cc3374fdc40921052efcc2e1f2ca3e416cf
urbaxadeeznuts <heee3767@gmail.com>
2022-07-16 13:00:12 +0200

    POV: Severe brain damage

--- python/emastalianReplacor.py
+++ python/emastalianReplacor.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     for filename in os.listdir("common"):
        with open(os.path.join("common", filename), 'r') as f:
        text = f.read()
-       text = text.replace("East_Emastalian","East_Maxan")
-       text = text.replace("West_Emastalian","West_Maxan")
+       text = text.replace("East_Emastalian","Maxan_East_Emastalian")
+       text = text.replace("West_Emastalian","Maxan_West_Emastalian")
        f.write(text)
